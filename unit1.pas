7069b16247dc09a1faf24f9467b3b6633a823c76
Alonso-PC\Alonso <alonsoaman@hotmail.com>
2017-09-18 21:38:00 -0300

    First Commit: initialization, pinMode e DigitalPinValue working.

---
+++ unit1.pas
@@ -0,0 +1,44 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, synaser, FirmataPascal;
+
+type
+  TForm1 = class(TForm)
+  private
+    { private declarations }
+
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+  piscadas: integer;
+
+implementation
+
+{$R *.lfm}
+    begin
+
+      initializeComm;
+
+      setPinMode($0D,$01);
+      piscadas := 1000;
+      while piscadas > 0 do
+           begin
+              digitalWrite($0D,$01);
+              sleep(60);
+              digitalWrite($0D,$00);
+              sleep(20);
+              piscadas -=1;
+           end;
+
+
+  end.
+
